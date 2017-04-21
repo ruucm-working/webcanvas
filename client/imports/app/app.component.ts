ddd8f0180d82df3fab530b06777ae6f1ce8c7aac
YONG JI <bumbermin@gmail.com>
2017-04-21 22:56:59 +0900

    (CSS) modify text css of main loading effect

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -6,13 +6,13 @@ import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { WordsComponent } from './_word/words.component';
 import {
-    Router,
-    // import as RouterEvent to avoid confusion with the DOM Event
-    Event as RouterEvent,
-    NavigationStart,
-    NavigationEnd,
-    NavigationCancel,
-    NavigationError
+	Router,
+	// import as RouterEvent to avoid confusion with the DOM Event
+	Event as RouterEvent,
+	NavigationStart,
+	NavigationEnd,
+	NavigationCancel,
+	NavigationError
 } from '@angular/router'
 
 @Component({
@@ -22,8 +22,6 @@ import {
 	providers: [ WordsComponent ]
 })
 export class MyCanvas {
-	// Sets initial value to true to show loading spinner on first load
-    loading: boolean = true;
 
 	constructor( 
 		private translate: TranslateService, 
@@ -34,30 +32,20 @@ export class MyCanvas {
 		let browserLang = translate.getBrowserLang();
 		translate.use(browserLang.match(/en|kr/) ? browserLang : 'en');
 		router.events.subscribe((event: RouterEvent) => {
-            this.navigationInterceptor(event);
-        });
+			this.navigationInterceptor(event);
+		});
 	}
-	// Shows and hides the loading spinner during RouterEvent changes
-    navigationInterceptor(event: RouterEvent): void {
-        if (event instanceof NavigationStart) {
-            this.loading = true;
-        }
-        if (event instanceof NavigationEnd) {
-            this.loading = false;
-            // $(".loading").addClass("loading_end");
-            setTimeout(function(){ $(".loading").addClass("loading_end"); }, 3000);
-        }
-
-        // Set loading state to false in both of the below events to hide the spinner in case a request fails
-        if (event instanceof NavigationCancel) {
-            this.loading = false;
-            // $(".loading").addClass("loading_end");
-            setTimeout(function(){ $(".loading").addClass("loading_end"); }, 3000);
-        }
-        if (event instanceof NavigationError) {
-            this.loading = false;
-            // $(".loading").addClass("loading_end");
-            setTimeout(function(){ $(".loading").addClass("loading_end"); }, 3000);
-        }
-    }
+	navigationInterceptor(event: RouterEvent): void {
+		if (event instanceof NavigationStart) {
+		}
+		if (event instanceof NavigationEnd) {
+			setTimeout(function(){ $(".loading").addClass("loading_end"); }, 2100);
+		}
+		if (event instanceof NavigationCancel) {
+			setTimeout(function(){ $(".loading").addClass("loading_end"); }, 2100);
+		}
+		if (event instanceof NavigationError) {
+			setTimeout(function(){ $(".loading").addClass("loading_end"); }, 2100);
+		}
+	}
 }
