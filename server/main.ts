1fb31d6fc067673ebc68917d2cb4328394128660
YONG JI <bumbermin@gmail.com>
2017-05-02 22:40:44 +0900

    (F) add kr.json text on home.compoenent

--- server/main.ts
+++ server/main.ts
@@ -15,18 +15,12 @@ Meteor.publish('canvascontents', function() {
 	return CanvasContents.find(); 
 });
 
-
 // Only publish files owned by this userId, and ignore
 // file chunks being used by Resumable.js for current uploads
 Meteor.publish('myData',
 function () {
-  // if (clientUserId === this.userId) {
 	return MyFiles.find({ 'metadata._Resumable': { $exists: false },
-						  'metadata.owner': this.userId });
- //  } else {        // Prevent client race condition:
-	// return null;  // This is triggered when publish is rerun with a new
-	// 			  // userId before client has resubscribed with that userId
- //  }
+						'metadata.owner': this.userId });
 }
 );
 
@@ -35,24 +29,24 @@ function () {
 MyFiles.allow({
 // The creator of a file owns it. UserId may be null.
 insert: function (userId, file) {
-  // Assign the proper owner when a file is created
-  file.metadata = file.metadata || {};
-  file.metadata.owner = userId;
-  return true;
+	// Assign the proper owner when a file is created
+	file.metadata = file.metadata || {};
+	file.metadata.owner = userId;
+	return true;
 },
 // Only owners can remove a file
 remove: function (userId, file) {
-  // Only owners can delete
-  return (userId === file.metadata.owner);
+	// Only owners can delete
+	return (userId === file.metadata.owner);
 },
 // Only owners can retrieve a file via HTTP GET
 read: function (userId, file) {
-  return (userId === file.metadata.owner);
+	return (userId === file.metadata.owner);
 },
 // This rule secures the HTTP REST interfaces' PUT/POST
 // Necessary to support Resumable.js
 write: function (userId, file, fields) {
-  // Only owners can upload file data
-  return (userId === file.metadata.owner);
+	// Only owners can upload file data
+	return (userId === file.metadata.owner);
 }
 });
