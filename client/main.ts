e10cf0729ec0599be005fa7511e6ef479ceba5f3
YONG JI <bumbermin@gmail.com>
2017-05-03 00:32:56 +0900

    (I-F) Enable Production Mode of Angular

--- client/main.ts
+++ client/main.ts
@@ -3,7 +3,9 @@ import 'reflect-metadata';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { Meteor } from 'meteor/meteor';
 import { AppModule } from './imports/app/app.module';
+import {enableProdMode} from '@angular/core';
 
+enableProdMode();
 Meteor.startup(() => {
 	platformBrowserDynamic().bootstrapModule(AppModule);
 });
